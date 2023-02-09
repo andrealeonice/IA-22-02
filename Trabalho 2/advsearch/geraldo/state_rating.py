3813e3f2344a41605e2ef798f1aa71fba53b2435
andrea <andrea_leonice@hotlmail.com>
2023-02-09 02:35:58 -0300

    adding improvements

--- Trabalho 2/advsearch/geraldo/state_rating.py
+++ Trabalho 2/advsearch/geraldo/state_rating.py
@@ -10,13 +10,38 @@ def rate_state(state: GameState) -> int:
     numPieces = state.board.piece_count[playerColor]
     opponentPieces = state.board.piece_count[opponentColor]
     piecesDiff = numPieces - opponentPieces
-    piecesWeight = 0.4
+    piecesWeight = 0.2
 
     # Rate by the number of valid moves
 
     numValidMovesPlayer = len(state.legal_moves())
     numValidMovesOpponent = len(GameState(state.board, opponentColor).legal_moves())
     validMovesyDiff = numValidMovesPlayer - numValidMovesOpponent
-    validMovesWeight = 0.6
+    validMovesWeight = 0.3
 
-    return piecesDiff * piecesWeight + validMovesyDiff * validMovesWeight
+    #Rate by the corners
+
+    numCornersPlayer=0
+    numCornersOpponent=0
+
+    if (state.board.is_legal((0,0),playerColor)):
+        numCornersPlayer += 5
+    elif (state.board.is_legal((0,0),opponentColor)):
+        numCornersOpponent+=5
+    if (state.board.is_legal((7,7),playerColor)):
+        numCornersPlayer += 5
+    elif (state.board.is_legal((7,7),opponentColor)):
+        numCornersOpponent += 5
+    if (state.board.is_legal((0, 7), playerColor)):
+        numCornersPlayer += 5
+    elif (state.board.is_legal((0, 7), opponentColor)):
+        numCornersOpponent += 5
+    if (state.board.is_legal((7, 0), playerColor)):
+        numCornersPlayer += 5
+    elif (state.board.is_legal((7, 0), opponentColor)):
+        numCornersOpponent += 5
+
+    cornnersDiff = numCornersPlayer - numCornersOpponent
+    validCornersWeight = 0.5
+
+    return piecesDiff * piecesWeight + validMovesyDiff * validMovesWeight + cornnersDiff * validCornersWeight
